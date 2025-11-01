82e4faaff11086a86db67e4c16e4996c3ac30fc7
MarioMastr <ashrmoha11@gmail.com>
2025-11-01 17:59:07 +0300

    completely reformat options to be a lot more easy on the eye

--- build.zig
+++ build.zig
@@ -20,17 +20,17 @@ pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const retroRevision = b.option(u8, "retro_revision", "What revision to compile for. Defaults to v5U = 3") orelse 3;
-    const retroUseModLoader = b.option(bool, "retro_use_mod_loader", "Enables or disables the mod loader.") orelse false;
-    const retroModLoaderVer = b.option(u8, "retro_mod_loader_ver", "Sets the mod loader version. Defaults to latest") orelse 2;
-    const gameIncludeEditor = b.option(bool, "game_include_editor", "Whether or not to include editor functions. Defaults to true") orelse true;
-    const maniaFirstRelease = b.option(bool, "mania_first_release", "Whether or not to build Mania's first release. Defaults to false") orelse false;
+    const retroRevision       = b.option(u8,   "retro_revision",       "What revision to compile for. Defaults to v5U = 3")                orelse 3;
+    const retroUseModLoader = b.option(bool, "retro_use_mod_loader", "Enables or disables the mod loader.")                              orelse false;
+    const retroModLoaderVer   = b.option(u8,   "retro_mod_loader_ver", "Sets the mod loader version. Defaults to latest")                  orelse 2;
+    const gameIncludeEditor = b.option(bool, "game_include_editor",  "Whether or not to include editor functions. Defaults to true")     orelse true;
+    const maniaFirstRelease = b.option(bool, "mania_first_release",  "Whether or not to build Mania's first release. Defaults to false") orelse false;
 
     var maniaPrePlus: bool = undefined;
     if (maniaFirstRelease) {
         maniaPrePlus = true;
     } else {
-        maniaPrePlus = b.option(bool, "mania_pre_plus", "Whether or not to build Mania pre-plus. Defaults to false") orelse false;
+        maniaPrePlus              = b.option(bool, "mania_pre_plus",       "Whether or not to build Mania pre-plus. Defaults to false")        orelse false;
     }
 
     var GAME_VERSION: u8 = undefined;
@@ -41,12 +41,12 @@ pub fn build(b: *std.Build) !void {
     }
 
     const add = b.addOptions();
-    add.addOption(u8, "retro_revision", retroRevision);
+    add.addOption(u8,   "retro_revision",       retroRevision);
     add.addOption(bool, "retro_use_mod_loader", retroUseModLoader);
-    add.addOption(u8, "retro_mod_loader_ver", retroModLoaderVer);
-    add.addOption(bool, "game_include_editor", gameIncludeEditor);
-    add.addOption(bool, "mania_pre_plus", maniaPrePlus);
-    add.addOption(bool, "mania_first_release", maniaFirstRelease);
+    add.addOption(u8,   "retro_mod_loader_ver", retroModLoaderVer);
+    add.addOption(bool, "game_include_editor",  gameIncludeEditor);
+    add.addOption(bool, "mania_pre_plus",       maniaPrePlus);
+    add.addOption(bool, "mania_first_release",  maniaFirstRelease);
 
     const selectedOptions = Options {
         .RETRO_REVISION = retroRevision,
@@ -86,34 +86,34 @@ pub fn build(b: *std.Build) !void {
         lib.root_module.addCMacro("_CRT_SECURE_NO_WARNINGS", "1");
     }
 
-    var retroRevBuf: [1]u8 = undefined;
-    _ = try std.fmt.bufPrint(&retroRevBuf, "{}", .{selectedOptions.RETRO_REVISION});
+    const retroRevBuf  = try std.fmt.allocPrint(b.allocator, "{}", .{selectedOptions.RETRO_REVISION});
+    defer b.allocator.free(retroRevBuf);
 
-    var modLoaderBuf: [1]u8 = undefined;
-    _ = try std.fmt.bufPrint(&modLoaderBuf, "{}", .{@intFromBool(selectedOptions.RETRO_USE_MOD_LOADER)});
+    const modLoaderBuf = try std.fmt.allocPrint(b.allocator, "{}", .{@intFromBool(selectedOptions.RETRO_MOD_LOADER)});
+    defer b.allocator.free(modLoaderBuf);
 
-    var modVerBuf: [1]u8 = undefined;
-    _ = try std.fmt.bufPrint(&modVerBuf, "{}", .{selectedOptions.RETRO_MOD_LOADER_VER});
+    const modVerBuf    = try std.fmt.allocPrint(b.allocator, "{}", .{selectedOptions.RETRO_MOD_LOADER_VER});
+    defer b.allocator.free(modVerBuf);
 
-    var editorBuf: [1]u8 = undefined;
-    _ = try std.fmt.bufPrint(&editorBuf, "{}", .{@intFromBool(selectedOptions.GAME_INCLUDE_EDITOR)});
+    const editorBuf    = try std.fmt.allocPrint(b.allocator, "{}", .{@intFromBool(selectedOptions.GAME_INCLUDE_EDITOR)});
+    defer b.allocator.free(editorBuf);
 
-    var prePlusBuf: [1]u8 = undefined;
-    _ = try std.fmt.bufPrint(&prePlusBuf, "{}", .{@intFromBool(selectedOptions.MANIA_PREPLUS)});
+    const prePlusBuf   = try std.fmt.allocPrint(b.allocator, "{}", .{@intFromBool(selectedOptions.MANIA_PREPLUS)});
+    defer b.allocator.free(prePlusBuf);
 
-    var firstRelBuf: [1]u8 = undefined;
-    _ = try std.fmt.bufPrint(&firstRelBuf, "{}", .{@intFromBool(selectedOptions.MANIA_FIRST_RELEASE)});
+    const firstRelBuf  = try std.fmt.allocPrint(b.allocator, "{}", .{@intFromBool(selectedOptions.MANIA_FIRST_RELEASE)});
+    defer b.allocator.free(firstRelBuf);
+  
+    const gameVerBuf   = try std.fmt.allocPrint(b.allocator, "{}", .{GAME_VERSION});
+    defer b.allocator.free(gameVerBuf);
 
-    var gameVerBuf: [1]u8 = undefined;
-    _ = try std.fmt.bufPrint(&gameVerBuf, "{}", .{GAME_VERSION});
-
-    lib.root_module.addCMacro("RETRO_REVISION", &retroRevBuf);
-    lib.root_module.addCMacro("RETRO_USE_MOD_LOADER", &modLoaderBuf);
-    lib.root_module.addCMacro("RETRO_MOD_LOADER_VER", &modVerBuf);
-    lib.root_module.addCMacro("GAME_INCLUDE_EDITOR", &editorBuf);
-    lib.root_module.addCMacro("MANIA_PREPLUS", &prePlusBuf);
-    lib.root_module.addCMacro("MANIA_FIRST_RELEASE", &firstRelBuf);
-    lib.root_module.addCMacro("GAME_VERSION", &gameVerBuf);
+    lib.root_module.addCMacro("RETRO_REVISION", retroRevBuf);
+    lib.root_module.addCMacro("RETRO_USE_MOD_LOADER", modLoaderBuf);
+    lib.root_module.addCMacro("RETRO_MOD_LOADER_VER", modVerBuf);
+    lib.root_module.addCMacro("GAME_INCLUDE_EDITOR", editorBuf);
+    lib.root_module.addCMacro("MANIA_PREPLUS", prePlusBuf);
+    lib.root_module.addCMacro("MANIA_FIRST_RELEASE", firstRelBuf);
+    lib.root_module.addCMacro("GAME_VERSION", gameVerBuf);
 
     lib.linkLibC();
     b.installArtifact(lib);
