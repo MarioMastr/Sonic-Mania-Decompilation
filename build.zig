fc46920655e9821ba5e33b6078f185eb2d1e53f7
MarioMastr <84964352+MarioMastr@users.noreply.github.com>
2025-11-03 07:28:19 +0300

    this makes more sense i think

--- build.zig
+++ build.zig
@@ -26,7 +26,7 @@ pub fn build(b: *std.Build) !void {
     const gameIncludeEditor = b.option(bool, "game_include_editor",  "Whether or not to include editor functions. Defaults to true")     orelse true;
     const maniaFirstRelease = b.option(bool, "mania_first_release",  "Whether or not to build Mania's first release. Defaults to false") orelse false;
     const maniaPrePlus      = if (maniaFirstRelease) true else b.option(bool, "mania_pre_plus", "Whether or not to build Mania pre-plus. Defaults to false") orelse false;
-    const GAME_VERSION: u8  = if (!maniaPrePlus) 6 else 3;
+    const GAME_VERSION: u8  = if (maniaPrePlus) 3 else 6;
 
     const add = b.addOptions();
     add.addOption(u8,   "retro_revision",       retroRevision);
